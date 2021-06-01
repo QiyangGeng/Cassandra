08d1e4ddf56a2d6117000000daab5afa4939eed7
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2021-06-01 10:49:09 -0700

    Initial implementation

--- src/main/java/com/efonian/cassandra/discord/commands/cmd_Info.java
+++ src/main/java/com/efonian/cassandra/discord/commands/cmd_Info.java
@@ -1,4 +1,44 @@
 package com.efonian.cassandra.discord.commands;
 
-class cmd_Info {
+import com.efonian.cassandra.discord.commands.annotation.DeclareCommandAccessLevel;
+import com.efonian.cassandra.util.UtilRuntime;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
+import java.util.Locale;
+
+@Component
+@DeclareCommandAccessLevel(accessLevel = CommandAccessLevel.ADMIN)
+class cmd_Info extends Command {
+    @Override
+    void execute(CommandContainer cc) {
+        cc.event.getChannel().sendMessage(new EmbedBuilder() {{
+            setTitle(getSelfName());
+            setThumbnail(getSelfAvatar());
+            
+            addField("Start time", DateTimeFormatter
+                    .ofLocalizedDate(FormatStyle.FULL)
+                    .withLocale(Locale.CANADA)
+                    .withZone(ZoneId.systemDefault())
+                    .format(UtilRuntime.getStartTime()), false);
+            addField("Hmm", "Yeah I'm not tracking anything else right now", false);
+            
+            setTimestamp(Instant.now());
+        }}.build()).queue();
+    }
+    
+    @Override
+    List<String> invokes() {
+        return List.of("info");
+    }
+    
+    @Override
+    String description() {
+        return "displays information on the bot";
+    }
 }
