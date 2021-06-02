b1f79a90e6d12a2cfae0079433e1184649e582ca
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2021-06-01 21:09:14 -0700

    Changed implementation to show prettier message while calculating ping

--- src/main/java/com/efonian/cassandra/discord/commands/cmd_Ping.java
+++ src/main/java/com/efonian/cassandra/discord/commands/cmd_Ping.java
@@ -10,7 +10,8 @@ import java.util.List;
 class cmd_Ping extends Command {
     @Override
     void execute(CommandContainer cc) {
-        cc.event.getChannel().sendMessage("_ _").queue(message ->
+        cc.event.getChannel().sendMessage(simpleEmbedBuilder("Pong!")
+                .setDescription(":stopwatch: --- ms").build()).queue(message ->
                 cc.event.getChannel().editMessageById(message.getIdLong(), simpleEmbedBuilder("Pong!")
                         .setDescription(":stopwatch: " + (message.getTimeCreated().toInstant().toEpochMilli()
                                 - cc.event.getMessage().getTimeCreated().toInstant().toEpochMilli()) + "ms")
