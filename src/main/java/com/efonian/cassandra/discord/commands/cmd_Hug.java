e9c17702ac9dbebdb83ecc82734b4ec6b56d7a16
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2021-06-01 23:32:35 -0700

    General refactor

--- src/main/java/com/efonian/cassandra/discord/commands/cmd_Hug.java
+++ src/main/java/com/efonian/cassandra/discord/commands/cmd_Hug.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -19,14 +18,12 @@ public class cmd_Hug extends Command {
     @Override
     void execute(CommandContainer cc) {
         User target = cc.event.getAuthor();
-        
-        if(!cc.args.isEmpty()) {
-            if(cc.args.get(0).chars().allMatch(Character::isDigit)) {
-                User potentialTarget = cc.event.getJDA().retrieveUserById(cc.args.get(0)).complete();
-                target = potentialTarget == null? target : potentialTarget;
-            } else if(cc.event.getMessage().getMentionedUsers().size() > 0) {
-                target = cc.event.getMessage().getMentionedUsers().get(0);
-            }
+    
+        if(cc.event.getMessage().getMentionedUsers().size() > 0)
+            target = cc.event.getMessage().getMentionedUsers().get(0);
+        else if(!cc.args.isEmpty()) {
+            User potentialTarget = cc.event.getJDA().retrieveUserById(cc.args.get(0)).complete();
+            target = potentialTarget == null ? target : potentialTarget;
         }
         
         cc.event.getChannel().sendMessage("Slowly approaching " + target.getName() + "...").queue();
@@ -47,15 +44,26 @@ public class cmd_Hug extends Command {
         A {
             private static final String TOP = "com/efonian/cassandra/discord/commands/hug/A/0.png";
             private static final String BASE = "com/efonian/cassandra/discord/commands/hug/A/1.png";
+            private static final int TARGET_SIZE = 230;
+            private static final int THETA = 12;
+            private static final int X_OFFSET = 160;
+            private static final int Y_OFFSET = 314;
+            
+            private BufferedImage top;
+            private BufferedImage base;
             
             @Override
-            ByteArrayOutputStream hug(User target) {
+            protected ByteArrayOutputStream hug(User target) {
                 try {
+                    // Loading assets
+                    BufferedImage top = getTop();
+                    BufferedImage base = getBase();
+                    BufferedImage avatar = ImageIO.read(new URL(target.getEffectiveAvatarUrl()));
+                    
+                    // Generating product
                     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                    BufferedImage top = ImageIO.read(new FileInputStream(cmd_Hug.class.getClassLoader().getResource(TOP).getPath()));
-                    BufferedImage base = ImageIO.read(new FileInputStream(cmd_Hug.class.getClassLoader().getResource(BASE).getPath()));
-                    BufferedImage icon = new BufferedImage(top.getWidth(), top.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                    icon.createGraphics().drawImage(formatIcon(UtilImage.readImage(new URL(target.getEffectiveAvatarUrl())), 230, 12), 160, 314, null);
+                    BufferedImage icon = new BufferedImage(base.getWidth(), base.getHeight(), BufferedImage.TYPE_INT_ARGB);
+                    icon.createGraphics().drawImage(formatIcon(avatar), X_OFFSET, Y_OFFSET, null);
                     ImageIO.write(UtilImage.stackImages(base, icon, top), "png", bytes);
                     bytes.close();
                     return bytes;
@@ -64,23 +72,37 @@ public class cmd_Hug extends Command {
                 }
             }
             
-            private BufferedImage formatIcon(BufferedImage icon, int targetSize, int theta) {
-                if (icon.getWidth() > targetSize)
-                    icon = UtilImage.ResizeTool.NEAREST_NEIGHBOR.resize(icon, targetSize, targetSize);
-                else if(icon.getWidth() < targetSize)
-                    icon = UtilImage.ResizeTool.KAIFU2X.resize(icon, targetSize, targetSize);
+            private BufferedImage formatIcon(BufferedImage icon) {
+                if (icon.getWidth() > TARGET_SIZE)
+                    icon = UtilImage.ResizeTool.NEAREST_NEIGHBOR.resize(icon, TARGET_SIZE, TARGET_SIZE);
+                else if(icon.getWidth() < TARGET_SIZE)
+                    icon = UtilImage.ResizeTool.KAIFU2X.resize(icon, TARGET_SIZE, TARGET_SIZE);
                 
-                return UtilImage.rotateImage(icon, theta);
+                return UtilImage.rotateImage(icon, THETA);
+            }
+            
+            private BufferedImage getTop() throws IOException {
+                URL url = cmd_Hug.class.getClassLoader().getResource(TOP);
+                if(url == null)
+                    throw new IOException("Cannot find top image for hug A");
+                return top == null ? (top = ImageIO.read(url)) : top;
+            }
+    
+            private BufferedImage getBase() throws IOException {
+                URL url = cmd_Hug.class.getClassLoader().getResource(BASE);
+                if(url == null)
+                    throw new IOException("Cannot find base image for hug A");
+                return base == null ? (base = ImageIO.read(url)) : base;
             }
         },
         S {
             @Override
-            ByteArrayOutputStream hug(User target) {
+            protected ByteArrayOutputStream hug(User target) {
                 // TODO
                 return null;
             }
         };
         
-        abstract ByteArrayOutputStream hug(User target);
+        protected abstract ByteArrayOutputStream hug(User target);
     }
 }
