3b2c1e12915eeb2dea17409733a2e52159d78197
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2021-06-06 18:42:29 -0700

    Overloaded readAll method to accept files

--- src/main/java/com/efonian/cassandra/util/UtilFile.java
+++ src/main/java/com/efonian/cassandra/util/UtilFile.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -25,6 +26,21 @@ public final class UtilFile {
             targetFile = new String(Files.readAllBytes(Paths.get(path)));
         } catch(IOException e) {
             logger.warn("Caught IOException when trying to read file with path: " + path);
+            e.printStackTrace();
+        }
+        return targetFile;
+    }
+    
+    /**
+     * Should only really be used if too lazy to catch exceptions, reads all bytes of a file as string, no checks
+     */
+    @Nullable
+    public static String readAll(File file) {
+        String targetFile = null;
+        try {
+            targetFile = new String(Files.readAllBytes(file.toPath()));
+        } catch(IOException e) {
+            logger.warn("Caught IOException when trying to read file with path: " + file.getPath());
             e.printStackTrace();
         }
         return targetFile;
