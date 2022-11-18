a161373547a9e5d604ed454d4d6a91f03f1424e2
QiyangGeng <75600357+QiyangGeng@users.noreply.github.com>
2022-11-17 20:13:58 -0800

    Renamed class

---
+++ src/main/java/com/efonian/cassandra/discord/audio/AudioManager.java
@@ -0,0 +1,5 @@
+package com.efonian.cassandra.discord.audio;
+
+// throw lavaplayer stuff here
+public final class AudioManager {
+}
--- src/main/java/com/efonian/cassandra/discord/guild/GuildAudioManager.java
+++
@@ -1,5 +0,0 @@
-package com.efonian.cassandra.discord.guild;
-
-// throw lavaplayer stuff here
-public final class GuildAudioManager {
-}
